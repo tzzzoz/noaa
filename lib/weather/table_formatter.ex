664482dc113c86688ea1b6e5c643f7208f334275
baicheng <baicheng@followanalytics.com>
2017-03-19 18:36:10 +0100

    reuse TableFormatter of the github issue project

---
+++ lib/weather/table_formatter.ex
@@ -0,0 +1,49 @@
+defmodule Weather.TableFormatter do
+  @moduledoc """
+  Format the list of issues into a delicate table.
+  It splits rows by columns then get the appropriate 
+  widths for each column. According to the widths of columns,
+  it can generate the format for columns, by using this format,
+  it is be able to print the final table.
+  """
+  import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
+  def print_table_for_columns(rows, headers) do
+    with data_by_columns = split_into_columns(rows, headers),
+         column_widths   = widths_of(data_by_columns),
+         format          = format_for(column_widths)
+    do
+      puts_one_line_in_columns(headers, format)
+      IO.puts(separator(column_widths))
+      puts_in_columns(data_by_columns, format)
+    end
+  end
+
+  def split_into_columns(rows, headers) do
+    for header <- headers do 
+      for row <- rows, do: row[header] |> to_string
+    end
+  end
+
+  def widths_of(columns) do
+    for column <- columns, do: column |> map(&String.length/1) |> max
+  end
+
+  def format_for(column_widths) do
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
+  end
+
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+  end
+
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
+
+  def puts_one_line_in_columns(fields, format) do
+    :io.format(format, fields)
+  end
+end
