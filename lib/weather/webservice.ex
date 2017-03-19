796be3754408e0cb74753f3ebaf9c908092b252e
baicheng <baicheng@followanalytics.com>
2017-03-19 18:28:49 +0100

    correct concatenating of strings with <>

--- lib/weather/webservice.ex
+++ lib/weather/webservice.ex
@@ -3,30 +3,31 @@ defmodule Weather.Webservice do
   @base_url Application.get_env(:weather, :base_url)
 
   def datasets_url(count) do
-    @base_url + "datasets?limit=#{count}"
+    @base_url <> "datasets?limit=#{count}"
   end
 
   def locations_url(count) do
-    @base_url + "locations?limit=#{count}"
+    @base_url <> "locations?limit=#{count}"
   end
 
   def data_url(params = %{}) do
     query =  Enum.reduce(params, "", fn({k, v}, acc)-> acc <> v end)
-    @base_url + query
+    @base_url <> query
   end
 
   def fetch(url) do
+    headers = [token: @token]
     url
-    |> HTTPoison.get
+    |> HTTPoison.get(headers)
     |> handle_response
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, Poison.Parse.parse!(body)}
+    {:ok, Poison.Parser.parse!(body)}
   end
 
   def handle_response({_, %{status_code: status, body: body}}) do
-    {:error, Poison.Parse.parse!(body)}
+    {:error, Poison.Parser.parse!(body)}
   end
 
   def handle_response({_error_code, %HTTPoison.Error{id: nil, reason: reason}}) do
